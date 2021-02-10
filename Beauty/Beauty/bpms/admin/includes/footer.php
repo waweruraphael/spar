552f594b96d1aa4a739d1bff4bf7cab979cea1d3
waweruraphael <weshraphael42@yahoo.com>
2021-02-10 20:58:54 +0300

    spar

---
+++ Beauty/Beauty/bpms/admin/includes/footer.php
@@ -0,0 +1,7 @@
+<!--footer-->
+    <div class="footer">
+    <p>
+        <span style="color: indigo;font-size: 15px">&copy;<?php echo date('Y'); ?>-All Rights Reserved-powered by Ralph_Tech</span></p>
+       
+    </div>
+        <!--//footer-->
